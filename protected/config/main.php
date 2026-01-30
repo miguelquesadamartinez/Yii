1dd83e09a98dd4d55749d2e136dc106167c44193
miguelquesadamartinez <miguel.quesada.martinez.1975@gmail.com>
2026-01-30 15:24:55 +0100

    Add info log

--- protected/config/main.php
+++ protected/config/main.php
@@ -75,12 +75,11 @@ return array(
         'log'=>array(
             'class'=>'CLogRouter',
             'routes'=>array(
-                /*
                 array(
                     'class'=>'CFileLogRoute',
-                    'levels'=>'error, warning',
+                    'levels'=>'error, warning, info, trace',
+                    'logFile'=>'application.log',
                 ),
-                */
                 // uncomment the following to show log messages on web pages
                 /*
                 array(
